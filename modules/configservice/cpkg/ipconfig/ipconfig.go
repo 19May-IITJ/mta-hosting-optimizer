021be723f5ce7d4cb6e1396993628b2cf0a5bd43
Mayank Singh <Mayank7.Singh@airtel.com>
2023-09-04 16:09:24 +0530

    comments

--- modules/configservice/cpkg/ipconfig/ipconfig.go
+++ modules/configservice/cpkg/ipconfig/ipconfig.go
@@ -5,3 +5,10 @@ type IPConfigData struct {
 	IPAddresses string `json:"ipAddresses"`
 	Status      bool   `json:"status"`
 }
+
+// type IPConfigData struct {
+// 	Hostname    string `json:"hostname" validate:"required,string"`
+// 	IPAddresses string `json:"ipAddresses" validate:"required,string"`
+// 	Status      bool   `json:"status" validate:"required,boolean"`
+// }
+// "github.com/go-playground/validator/v10"
